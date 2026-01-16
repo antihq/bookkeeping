d05a5b04e934b328ecd23e6e0fbc7eea27d91ed1
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-01-15 21:05:03 -0600

    ui: reduce vertical spacing on small screens so users can see more transactions on tablets and smaller laptops without excessive scrolling

--- resources/views/pages/accounts/⚡show.blade.php
+++ resources/views/pages/accounts/⚡show.blade.php
@@ -240,7 +240,7 @@ new #[Title('Account')] class extends Component {
     </div>
 
     <div class="space-y-14">
-        <div class="space-y-6">
+        <div class="space-y-6 sm:space-y-4">
             <div class="flex items-end justify-between gap-4">
                 <flux:heading size="lg">All transactions</flux:heading>
 
