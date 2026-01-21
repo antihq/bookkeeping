52285082ccb699e2f6e5c1ab24fa995a8c3ee814
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-01-21 07:26:55 -0600

    ui: align user profile to right edge of header on all screen sizes

--- resources/views/components/⚡header.blade.php
+++ resources/views/components/⚡header.blade.php
@@ -35,7 +35,7 @@ new class extends Component {
 <flux:header {{ $attributes }}>
     <flux:spacer class="max-sm:hidden" />
 
-    <flux:navbar class="overflow-x-auto whitespace-nowrap scrollbar-hide me-4">
+    <flux:navbar class="overflow-x-auto whitespace-nowrap scrollbar-hide max-sm:me-4">
         <flux:navbar.item
             :href="route('dashboard')"
             :current="request()->routeIs('dashboard')"
@@ -71,97 +71,96 @@ new class extends Component {
         </flux:navbar.item>
     </flux:navbar>
 
-    <flux:spacer />
+    <div class="flex flex-1 items-center justify-end">
+        <flux:dropdown position="top" align="end">
+            <flux:profile class="cursor-pointer" :chevron="false">
+                <x-slot:avatar>
+                    @if (auth()->user()->profile_photo_path)
+                        <flux:avatar :src="auth()->user()->profile_photo_url" size="xs" circle />
+                    @else
+                        <x-boring-avatar
+                            :name="auth()->user()->name ?? auth()->user()->email"
+                            variant="beam"
+                            class="[:where(&)]:size-7 sm:[:where(&)]:size-6"
+                        />
+                    @endif
+                </x-slot>
+            </flux:profile>
 
-    <!-- Desktop User Menu -->
-    <flux:dropdown position="top" align="end">
-        <flux:profile class="cursor-pointer" :chevron="false">
-            <x-slot:avatar>
-                @if (auth()->user()->profile_photo_path)
-                    <flux:avatar :src="auth()->user()->profile_photo_url" size="xs" circle />
-                @else
-                    <x-boring-avatar
-                        :name="auth()->user()->name ?? auth()->user()->email"
-                        variant="beam"
-                        class="[:where(&)]:size-7 sm:[:where(&)]:size-6"
-                    />
-                @endif
-            </x-slot>
-        </flux:profile>
-
-        <flux:menu class="min-w-64">
-            <flux:menu.group heading="Account">
-                <flux:menu.item :href="route('profile.edit')" icon="user" icon:variant="micro" wire:navigate>
-                    Profile
-                </flux:menu.item>
-                <flux:menu.item :href="route('user-password.edit')" icon="key" icon:variant="micro" wire:navigate>
-                    Password
-                </flux:menu.item>
-                <flux:menu.item :href="route('two-factor.show')" icon="shield-check" icon:variant="micro" wire:navigate>
-                    Two-factor auth
-                </flux:menu.item>
-                <flux:menu.item
-                    :href="route('devices.create')"
-                    icon="device-phone-mobile"
-                    icon:variant="micro"
-                    wire:navigate
-                >
-                    Devices
-                </flux:menu.item>
-                <flux:menu.item
-                    :href="route('appearance.edit')"
-                    icon="adjustments-horizontal"
-                    icon:variant="micro"
-                    wire:navigate
-                >
-                    Appearance
-                </flux:menu.item>
-            </flux:menu.group>
-
-            <flux:menu.group heading="Team">
-                <flux:menu.item
-                    :href="route('teams.edit', auth()->user()->currentTeam)"
-                    icon="cog-8-tooth"
-                    icon:variant="micro"
-                    wire:navigate
-                >
-                    Settings
-                </flux:menu.item>
-                <flux:menu.item
-                    :href="route('teams.members.index', auth()->user()->currentTeam)"
-                    icon="users"
-                    icon:variant="micro"
-                    wire:navigate
-                >
-                    Members
-                </flux:menu.item>
-                <flux:menu.item :href="route('teams.create')" icon="plus" icon:variant="micro" wire:navigate>
-                    Create new team
-                </flux:menu.item>
-            </flux:menu.group>
-
-            @if (Auth::user()->allTeams()->count() > 1)
-                <flux:menu.group heading="Switch teams">
-                    <flux:menu.radio.group wire:model.live="team">
-                        @foreach (Auth::user()->allTeams() as $team)
-                            <flux:menu.radio :value="$team->id">{{ $team->name }}</flux:menu.radio>
-                        @endforeach
-                    </flux:menu.radio.group>
+            <flux:menu class="min-w-64">
+                <flux:menu.group heading="Account">
+                    <flux:menu.item :href="route('profile.edit')" icon="user" icon:variant="micro" wire:navigate>
+                        Profile
+                    </flux:menu.item>
+                    <flux:menu.item :href="route('user-password.edit')" icon="key" icon:variant="micro" wire:navigate>
+                        Password
+                    </flux:menu.item>
+                    <flux:menu.item :href="route('two-factor.show')" icon="shield-check" icon:variant="micro" wire:navigate>
+                        Two-factor auth
+                    </flux:menu.item>
+                    <flux:menu.item
+                        :href="route('devices.create')"
+                        icon="device-phone-mobile"
+                        icon:variant="micro"
+                        wire:navigate
+                    >
+                        Devices
+                    </flux:menu.item>
+                    <flux:menu.item
+                        :href="route('appearance.edit')"
+                        icon="adjustments-horizontal"
+                        icon:variant="micro"
+                        wire:navigate
+                    >
+                        Appearance
+                    </flux:menu.item>
                 </flux:menu.group>
-            @endif
 
-            <form method="POST" action="{{ route('logout') }}" class="w-full">
-                @csrf
-                <flux:menu.item
-                    as="button"
-                    type="submit"
-                    icon="arrow-right-start-on-rectangle"
-                    icon:variant="micro"
-                    class="w-full"
-                >
-                    Log out
-                </flux:menu.item>
-            </form>
-        </flux:menu>
-    </flux:dropdown>
+                <flux:menu.group heading="Team">
+                    <flux:menu.item
+                        :href="route('teams.edit', auth()->user()->currentTeam)"
+                        icon="cog-8-tooth"
+                        icon:variant="micro"
+                        wire:navigate
+                    >
+                        Settings
+                    </flux:menu.item>
+                    <flux:menu.item
+                        :href="route('teams.members.index', auth()->user()->currentTeam)"
+                        icon="users"
+                        icon:variant="micro"
+                        wire:navigate
+                    >
+                        Members
+                    </flux:menu.item>
+                    <flux:menu.item :href="route('teams.create')" icon="plus" icon:variant="micro" wire:navigate>
+                        Create new team
+                    </flux:menu.item>
+                </flux:menu.group>
+
+                @if (Auth::user()->allTeams()->count() > 1)
+                    <flux:menu.group heading="Switch teams">
+                        <flux:menu.radio.group wire:model.live="team">
+                            @foreach (Auth::user()->allTeams() as $team)
+                                <flux:menu.radio :value="$team->id">{{ $team->name }}</flux:menu.radio>
+                            @endforeach
+                        </flux:menu.radio.group>
+                    </flux:menu.group>
+                @endif
+
+                <form method="POST" action="{{ route('logout') }}" class="w-full">
+                    @csrf
+                    <flux:menu.item
+                        as="button"
+                        type="submit"
+                        icon="arrow-right-start-on-rectangle"
+                        icon:variant="micro"
+                        class="w-full"
+                    >
+                        Log out
+                    </flux:menu.item>
+                </form>
+            </flux:menu>
+        </flux:dropdown>
+    </div>
 </flux:header>
